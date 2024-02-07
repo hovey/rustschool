fa9150fee2c30acc7d3fff437a7ed981f84baec0
Chad Hovey <hovey@me.com>
2024-02-07 10:44:18 -0700

    traits with Cat and Dog

--- cats_and_dogs/src/main.rs
+++ cats_and_dogs/src/main.rs
@@ -1,3 +1,7 @@
+// Output:
+// My name is Sylvester, language is meow.  My grayscale is 128.
+// My name is Fido, language is bark.  My RGB colors are 11, 22, 33.
+
 trait Describable {
     fn describe(&self) -> String {
         // default implementation
@@ -5,38 +9,68 @@ trait Describable {
     }
 }
 
-struct Person {
-    name: String,
-    age: u32,
+impl Describable for Cat {
+    fn describe(&self) -> String {
+        format!(
+            "My name is {}, language is {}.  My grayscale is {}.",
+            self.animal.name, self.animal.language, self.grayscale
+        )
+    }
 }
 
-impl Describable for Person {
+impl Describable for Dog {
     fn describe(&self) -> String {
-        format!("{} is {} years old", self.name, self.age)
+        format!(
+            "My name is {}, language is {}.  My RGB colors are {}, {}, {}.",
+            self.animal.name, self.animal.language, self.red, self.green, self.blue
+        )
     }
+}
+
+struct Animal {
+    name: String,
+    language: String,
+}
+
+struct Dog {
+    animal: Animal,
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+struct Cat {
+    animal: Animal,
+    grayscale: u32,
 }
 
 fn print_description(item: &impl Describable) {
     println!("{}", item.describe());
 }
 
-fn print_description_destructive(item: impl Describable) {
-    println!("{}", item.describe());
-}
-
 fn main() {
-    let person = Person {
-        name: "Adam".to_string(),
-        age: 20,
+    let a1 = Animal {
+        name: "Sylvester".to_string(),
+        language: "meow".to_string(),
     };
 
-    // demonstrate borrowing by moving the print statements out of the
-    // main
+    let a2 = Animal {
+        name: "Fido".to_string(),
+        language: "bark".to_string(),
+    };
 
-    // person gets destroyed after the print_description_destructive call
-    print_description_destructive(person);
+    let sylvester = Cat {
+        animal: a1,
+        grayscale: 128,
+    };
 
-    // since person is destroyed above, there is no longer a person to pass to
-    // print_description
-    print_description(&person);
+    let fido = Dog {
+        animal: a2,
+        red: 11,
+        green: 22,
+        blue: 33,
+    };
+
+    print_description(&sylvester);
+    print_description(&fido);
 }
