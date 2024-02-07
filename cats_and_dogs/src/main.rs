b48a0c164c78a956d12a8ad43b982679c87152cc
Michael R. Buche <dev@mrbuche.com>
2024-02-07 13:36:10 -0700

    Update main.rs

--- cats_and_dogs/src/main.rs
+++ cats_and_dogs/src/main.rs
@@ -5,69 +5,78 @@
 struct Dog {
     red: u32,
     green: u32,
-    blue: u32
+    blue: u32,
+    name: String,
+    language: String
 }
 
 struct Cat {
     grayscale: u32,
+    name: String,
+    language: String
 }
 
 trait Animal {
-    fn get_name(&self) -> String;
-    fn get_language(&self) -> String;
+    fn get_name(&self) -> &String;
+    fn get_language(&self) -> &String;
+    fn get_info(&self) -> String;
 }
 
 impl Animal for Dog {
-    fn get_name(&self) -> String {
-        "Fido".to_string()
+    fn get_name(&self) -> &String {
+        &self.name
     }
-    fn get_language(&self) -> String {
-        "bark".to_string()
+    fn get_language(&self) -> &String {
+        &self.language
+    }
+    fn get_info(&self) -> String
+    {
+        format!(
+            "My RGB colors are {}, {}, {}.",
+            self.red, self.green, self.blue
+        )
     }
 }
 
 impl Animal for Cat {
-    fn get_name(&self) -> String {
-        "Sylvester".to_string()
+    fn get_name(&self) -> &String {
+        &self.name
     }
-    fn get_language(&self) -> String {
-        "meow".to_string()
+    fn get_language(&self) -> &String {
+        &self.language
     }
-}
-
-trait Describable where Self: Animal {
-    fn describe(&self) -> String {
-        // default implementation
-        "Uninplemented".to_string()
-    }
-}
-
-impl Describable for Dog {
-    fn describe(&self) -> String {
+    fn get_info(&self) -> String {
         format!(
-            "My name is {}, language is {}.  My RGB colors are {}, {}, {}.",
-            self.get_name(), self.get_language(), self.red, self.green, self.blue
+            "My grayscale is {}.",
+            self.grayscale
         )
     }
 }
 
-impl Describable for Cat {
+trait Describable: Animal {
     fn describe(&self) -> String {
         format!(
-            "My name is {}, language is {}.  My grayscale is {}.",
-            self.get_name(), self.get_language(), self.grayscale
+            "My name is {}, language is {}. {}.",
+            self.get_name(), self.get_language(), self.get_info()
         )
     }
 }
+
+impl Describable for Cat {}
+impl Describable for Dog {}
 
 fn main() {
     let sylvester = Cat {
         grayscale: 128,
+        name: "Sylvester".to_string(),
+        language: "meow".to_string()
     };
     let fido = Dog {
         red: 11,
         green: 22,
         blue: 33,
+        name: "Fido".to_string(),
+        language: "bark".to_string()
     };
     println!("{}", sylvester.describe());
     println!("{}", fido.describe());
