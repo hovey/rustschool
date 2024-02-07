47c5c48d11e5481a543e6be6a9f52329742a0bde
Michael R. Buche <dev@mrbuche.com>
2024-02-07 14:45:45 -0700

    Update main.rs

--- cats_and_dogs/src/main.rs
+++ cats_and_dogs/src/main.rs
@@ -2,82 +2,68 @@
 // My name is Sylvester, language is meow.  My grayscale is 128.
 // My name is Fido, language is bark.  My RGB colors are 11, 22, 33.
 
-struct Dog {
-    red: u32,
-    green: u32,
-    blue: u32,
+struct Animal {
     name: String,
     language: String
 }
 
 struct Cat {
     grayscale: u32,
-    name: String,
-    language: String
+    animal: Animal
 }
 
-trait Animal {
-    fn get_name(&self) -> &String;
-    fn get_language(&self) -> &String;
-    fn get_info(&self) -> String;
+struct Dog {
+    red: u32,
+    green: u32,
+    blue: u32,
+    animal: Animal
 }
 
-impl Animal for Dog {
-    fn get_name(&self) -> &String {
-        &self.name
-    }
-    fn get_language(&self) -> &String {
-        &self.language
-    }
-    fn get_info(&self) -> String
-    {
-        format!(
-            "My RGB colors are {}, {}, {}.",
-            self.red, self.green, self.blue
-        )
-    }
+trait Describable {
+    fn describe(&self) -> String;
 }
 
-impl Animal for Cat {
-    fn get_name(&self) -> &String {
-        &self.name
-    }
-    fn get_language(&self) -> &String {
-        &self.language
-    }
-    fn get_info(&self) -> String {
-        format!(
-            "My grayscale is {}.",
-            self.grayscale
-        )
-    }
-}
-
-trait Describable: Animal {
+impl Describable for Cat {
     fn describe(&self) -> String {
         format!(
-            "My name is {}, language is {}. {}",
-            self.get_name(), self.get_language(), self.get_info()
+            "My name is {}, language is {}.  My grayscale is {}.",
+            self.animal.name, self.animal.language, self.grayscale
         )
     }
 }
 
-impl Describable for Cat {}
-impl Describable for Dog {}
+impl Describable for Dog {
+    fn describe(&self) -> String {
+        format!(
+            "My name is {}, language is {}.  My RGB colors are {}, {}, {}.",
+            self.animal.name, self.animal.language, self.red, self.green, self.blue
+        )
+    }
+}
+
+struct MyAnimals (
+    Cat,
+    Dog
+);
 
 fn main() {
     let sylvester = Cat {
+        animal: Animal {
+            name: "Sylvester".to_string(),
+            language: "meow".to_string(),
+        },
         grayscale: 128,
-        name: "Sylvester".to_string(),
-        language: "meow".to_string()
     };
     let fido = Dog {
+        animal: Animal {
+            name: "Fido".to_string(),
+            language: "bark".to_string(),
+        },
         red: 11,
         green: 22,
         blue: 33,
-        name: "Fido".to_string(),
-        language: "bark".to_string()
     };
-    println!("{}", sylvester.describe());
-    println!("{}", fido.describe());
+    let animals = MyAnimals(sylvester, fido);
+    println!("{}", animals.0.describe());
+    println!("{}", animals.1.describe());
 }
